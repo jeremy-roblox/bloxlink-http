c3c9cfba07d3f750353a3de8af4b2917a9b4c3a5
xomien <xomien01@gmail.com>
2021-11-30 02:13:14 -0500

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -2,9 +2,7 @@ from os import environ as env
 from resources.constants import DISCORD_PUBLIC_KEY, SERVER_HOST, SERVER_PORT
 from resources.structures import Bloxlink
 
-bot = Bloxlink(env.get("DISCORD_PUBLIC_KEY", DISCORD_PUBLIC_KEY))
-
-
 
 if __name__ == "__main__":
+    bot = Bloxlink(env.get("DISCORD_PUBLIC_KEY", DISCORD_PUBLIC_KEY))
     bot.run(SERVER_HOST, SERVER_PORT, debug=True)
