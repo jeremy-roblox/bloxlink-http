128681eb537da64804d7c95f83449590ccc4a134
xomien <xomien01@gmail.com>
2021-11-30 02:09:16 -0500

    minor

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,6 @@
 from os import environ as env
 from resources.constants import DISCORD_PUBLIC_KEY, SERVER_HOST, SERVER_PORT
-from resources.structures.Bloxlink import Bloxlink
+from resources.structures import Bloxlink
 
 bot = Bloxlink(env.get("DISCORD_PUBLIC_KEY", DISCORD_PUBLIC_KEY))
 
