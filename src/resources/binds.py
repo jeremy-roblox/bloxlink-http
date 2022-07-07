23fb1cb0a9d354f849704745e321df907bd860cf
xomien <xomien01@gmail.com>
2022-07-07 01:22:14 -0400

    Bind sets

--- src/resources/binds.py
+++ src/resources/binds.py
@@ -2,7 +2,8 @@ from .models import BloxlinkGuild
 import resources.users as users
 import resources.groups as groups
 from .bloxlink import instance as bloxlink
-from snowfin import User
+from snowfin import Member
+from typing import Tuple
 
 
 async def get_linked_group_ids(guild_id: int) -> set:
@@ -14,8 +15,11 @@ async def get_linked_group_ids(guild_id: int) -> set:
     return set(group_ids.keys()).union(set(role_binds.get("groups", {}).keys()))
 
 
-def check_bind_for(guild_roles: list, roblox_account: users.RobloxAccount, bind_type: str, bind_id: str, **bind_data) -> bool:
-    bind_roles = []
+def check_bind_for(guild_roles: list, roblox_account: users.RobloxAccount, bind_type: str, bind_id: str, **bind_data) -> Tuple[bool, set, set]:
+    bind_roles   = set()
+    remove_roles = set()
+
+    # success = False
 
     if bind_type == "group":
         if bind_data.get("roleset"):
@@ -30,14 +34,29 @@ def check_bind_for(guild_roles: list, roblox_account: users.RobloxAccount, bind_
                 else:
                     for role in guild_roles:
                         if role["managed"] == False and user_group.rolesets.get(role["name"]):
-                            bind_roles.append(role)
+                            bind_roles.add(role["id"])
+                            break
 
-                    return True, bind_roles
+                    return True, bind_roles, remove_roles
 
+    # if success:
+        # parse for valid roles
+        # if bind_data.get("removeRoles") or bind_data.get("roles"):
+        #     for role in guild_roles:
+        #         if role["managed"] == False:
+        #             if bind_data.get("removeRoles"):
+        #                 for remove_role in bind_data["removeRoles"]:
+        #                     if str(role["id"]) == remove_role:
+        #                         remove_roles.remove(remove_role)
 
-    return False, bind_roles
+        #             if bind_data.get("roles"):
+        #                 for remove_role in bind_data["roles"]:
+        #                     if role["id"] == remove_role:
+        #                         remove_roles.remove(remove_role)
 
-async def get_binds_for(user: User, guild_id: int, roblox_account: users.RobloxAccount = None) -> dict:
+    return False, bind_roles, remove_roles
+
+async def get_binds_for(member: Member, guild_id: int, roblox_account: users.RobloxAccount = None) -> dict:
     guild_binds = await bloxlink.fetch_guild(str(guild_id), "binds")
     role_binds = guild_binds.binds or []
 
@@ -57,8 +76,6 @@ async def get_binds_for(user: User, guild_id: int, roblox_account: users.RobloxA
 
     if roblox_account:
         for bind_data in role_binds:
-            # bind_add_roles    = bind_data.get("roles") or []
-            # bind_remove_roles = bind_data.get("removeRoles") or []
             # bind_nickname     = bind_data.get("nickname") or None
             role_bind         = bind_data.get("bind") or {}
             bind_criteria     = bind_data.get("criteria") or []
@@ -69,36 +86,42 @@ async def get_binds_for(user: User, guild_id: int, roblox_account: users.RobloxA
 
             success: bool = False
             bind_roles: list = []
+            bind_remove_roles: list = []
 
             if bind_criteria:
                 for bind_ in bind_criteria:
                     #check_bind_for()
                     raise NotImplementedError()
             else:
-                success, bind_roles = check_bind_for(guild_roles, roblox_account, bind_type, bind_id)
+                success, bind_roles, bind_remove_roles = check_bind_for(guild_roles, roblox_account, bind_type, bind_id)
                 # print(bind_roles)
-
 
             if success:
                 if bind_required:
-                    user_binds["required"].append([bind_data, bind_roles])
+                    user_binds["required"].append([bind_data, bind_roles, bind_remove_roles])
                 else:
-                    user_binds["optional"].append([bind_data, bind_roles])
+                    user_binds["optional"].append([bind_data, bind_roles, bind_remove_roles])
 
 
     return user_binds
 
 
 
-async def apply_binds(user: User, guild_id: int, roblox_account: users.RobloxAccount, *, moderate_user=False) -> None:
-    user_binds = await get_binds_for(user, guild_id, roblox_account)
+async def apply_binds(member: Member, guild_id: int, roblox_account: users.RobloxAccount, *, moderate_user=False) -> None:
+    user_binds = await get_binds_for(member, guild_id, roblox_account)
 
     print(user_binds)
 
     # first apply the required binds, then ask the user if they want to apply the optional binds
 
-    add_roles = []
-    remove_roles = []
+    add_roles    = set()
+    remove_roles = set()
 
     for required_bind in user_binds["required"]:
-        pass
+        add_roles.update(required_bind[1])
+        remove_roles.update(required_bind[2])
+
+    remove_roles = remove_roles.difference(add_roles) # added roles get priority
+    add_roles    = add_roles.difference(set([str(r) for r in member.roles])) # remove roles that are already on the user
+
+    return add_roles
