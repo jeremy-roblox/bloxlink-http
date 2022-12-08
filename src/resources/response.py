a7079ec078a0cd5b5d8f89e15c9308843ea5cb8f
zomien <xomien01@gmail.com>
2022-12-08 01:17:59 -0500

    Embed support

--- src/resources/response.py
+++ src/resources/response.py
@@ -3,29 +3,30 @@ import hikari
 
 
 class Response:
-    def __init__(self, interaction:hikari.CommandInteraction):
+    def __init__(self, interaction: hikari.CommandInteraction):
         self.interaction = interaction
         self._responded = False
         self._responded_once = False # if True, we respond to Discord with the content
-        self.responded_once_content = None # we respond to Discord with this
+        self.responded_once_content: dict | None = None # we respond to Discord with this
 
 
-    async def send(self, content, **kwargs):
+    async def send(self, content: str | None = None, embed: hikari.Embed | None = None, **kwargs):
         if self._responded:
             self.responded_once = False
             self.responded_once_content = None
-            await self.interaction.execute(content, **kwargs)
+            await self.interaction.execute(content, embed=embed, **kwargs)
         else:
             self._responded = True
             self.responded_once = True
             self.responded_once_content = {
                 "content": content,
+                "embed": embed,
                 **kwargs
             }
 
             await self.interaction.create_initial_response(
                 hikari.ResponseType.MESSAGE_CREATE,
-                content, **kwargs)
+                content, embed=embed, **kwargs)
 
     async def defer(self):
         if self._responded:
