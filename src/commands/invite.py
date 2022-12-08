fb7570bd4bd84e6ef2d49fcdf8c508975996f93b
zomien <xomien01@gmail.com>
2022-12-08 01:18:42 -0500

    Remove snowfin commands for now

--- src/commands/invite.py
+++ src/commands/invite.py
@@ -1,35 +1,22 @@
-from unicodedata import category
-from snowfin import Module, slash_command, Embed, EmbedFooter, Interaction, Button, EmbedField
-from resources.bloxlink import Bloxlink
+from resources.bloxlink import instance as bloxlink
+from resources.models import CommandContext
+from hikari import Embed, EmbedField
 
-from snowfin.embed import Empty
 
-class InviteCommand(Module):
-    category = "Miscellaneous"
+@bloxlink.command()
+class InviteCommand():
 
-    @slash_command("invite")
-    async def invite(self, ctx: Interaction):
+    async def __main__(self, ctx: CommandContext):
         """invite the bot to your server"""
 
-        print(dir(self.client.user))
-        print(self.client.user)
-
-        return (
+        await ctx.response.send(embed=
             Embed(
-                title="Invite Bloxlink",
-                description="To add Bloxlink to your server, click the button below.",
-                color=0xdb2323,
-                footer=EmbedFooter(
-                    text="Thanks for choosing Bloxlink!",
-                    icon_url=self.client.user.avatar_url if self.client.user else Empty
-                ),
-                fields=[EmbedField(
-                    name="Frequently Asked Questions",
-                    value="**Q:** I don't see my server when I invite the bot!" \
-                        "\n**A:** Make sure you have the `Manage Server` role permission and are logged into the correct account via https://discord.com"
-                )]
-            ),
-            Button("Invite Bloxlink", url="https://blox.link/invite"),
-            Button("Support Server", url="https://blox.link/support")
+                title="Test"
+            )
         )
 
+        await ctx.response.send(embed=
+            Embed(
+                title="Test"
+            )
+        )
