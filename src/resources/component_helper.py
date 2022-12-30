fbb24f53b4a77a495c9b6a6ce95e6aa3ba961eb6
zomien <xomien01@gmail.com>
2022-12-31 00:52:23 +0100

    Component helper utilities

---
+++ src/resources/component_helper.py
@@ -0,0 +1,59 @@
+import hikari
+from resources.bloxlink import instance as bloxlink
+
+async def get_component(message: hikari.Message, custom_id: str):
+    for action_row in message.components:
+        for component in action_row.components:
+            if component.custom_id.startswith(custom_id):
+                return component
+
+
+async def save_components(message: hikari.Message):
+    """Hikari requires a new builder for component edits"""
+
+    components = []
+
+    for action_row in message.components:
+        for component in action_row.components:
+            if isinstance(component, hikari.SelectMenuComponent):
+                new_select_menu = (
+                    bloxlink.rest.build_action_row().add_select_menu(component.custom_id)
+                        .set_placeholder(component.placeholder))
+
+                for option in component.options:
+                    new_select_menu = new_select_menu.add_option(option.label, option.value).add_to_menu()
+
+                new_select_menu = new_select_menu.add_to_container()
+
+                components.append(new_select_menu)
+
+    await message.edit(components=components)
+
+
+async def set_custom_id_data(message: hikari.Message, custom_id: str, segment: int, values: list):
+    component = await get_component(message, custom_id=custom_id)
+
+    if component:
+        custom_id_data = component.custom_id.split(":")
+        if len(custom_id_data) < segment:
+            for _ in range(segment-len(custom_id_data)):
+                custom_id_data.append("")
+
+            custom_id = ":".join(custom_id_data)
+
+
+        segment_data = (custom_id_data[segment-1] if len(custom_id_data) >= segment else "").split(",")
+
+        if segment_data[0] == "":
+            # fix blank lists
+            segment_data.pop(0)
+
+        for value in values:
+            value = value.strip()
+            if value not in segment_data:
+                segment_data.append(value)
+
+        custom_id_data[segment-1] = ",".join(segment_data)
+        component.custom_id = ":".join(custom_id_data)
+
+        await save_components(message)
