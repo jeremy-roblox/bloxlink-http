edd4a04d83a1db9e8652f33db31babfac434b79d
Nub <38899321+One-Nub@users.noreply.github.com>
2023-02-02 06:52:42 +0000

    Format with black, 110 line length

--- src/resources/binds.py
+++ src/resources/binds.py
@@ -11,30 +11,40 @@ import hikari
 import re
 
 
-
 nickname_template_regex = re.compile(r"\{(.*?)\}")
-any_group_nickname      = re.compile(r"\{group-rank-(.*?)\}")
-bracket_search          = re.compile(r"\[(.*)\]")
-
-
+any_group_nickname = re.compile(r"\{group-rank-(.*?)\}")
+bracket_search = re.compile(r"\[(.*)\]")
 
 
 async def count_binds(guild_id: int | str, group_id: int | str = None) -> int:
     guild_data: GuildData = await bloxlink.fetch_guild_data(str(guild_id), "binds")
 
-    return len(guild_data.binds) if not group_id else sum(1 for b in guild_data.binds if b["bind"]["id"] == int(group_id)) or 0
+    return (
+        len(guild_data.binds)
+        if not group_id
+        else sum(1 for b in guild_data.binds if b["bind"]["id"] == int(group_id)) or 0
+    )
+
 
 async def get_bind_desc(guild_id: int | str, group_id: int | str = None):
     return "TODO: show existing binds"
 
-async def parse_nickname(member: hikari.Member, guild: hikari.Guild, template: str, roblox_user=MISSING, group: groups.RobloxGroup=MISSING, is_nickname: bool=True) -> str | None:
+
+async def parse_nickname(
+    member: hikari.Member,
+    guild: hikari.Guild,
+    template: str,
+    roblox_user=MISSING,
+    group: groups.RobloxGroup = MISSING,
+    is_nickname: bool = True,
+) -> str | None:
     template = template or DEFAULTS.get("nicknameTemplate") or ""
 
     if template == "{disable-nicknaming}":
         return
 
     group_id = None
-    group    = None
+    group = None
     roblox_user = roblox_user or (roblox_user is not MISSING and (await users.get_user(user=member)))
 
     if roblox_user is not MISSING:
@@ -42,7 +52,9 @@ async def parse_nickname(member: hikari.Member, guild: hikari.Guild, template: s
 
         if group is MISSING:
             guild_data: GuildData = await bloxlink.fetch_guild_data(guild, "binds")
-            group_id = any(b["bind"]["type"] == "group" for b in guild_data.binds) if guild_data.binds else None
+            group_id = (
+                any(b["bind"]["type"] == "group" for b in guild_data.binds) if guild_data.binds else None
+            )
 
             if group_id:
                 group = roblox_user.groups.get(group_id)
@@ -67,7 +79,7 @@ async def parse_nickname(member: hikari.Member, guild: hikari.Guild, template: s
             #         if brackets_match:
             #             group_role_from_group = f"[{brackets_match.group(1)}]"
 
-            template = template.replace("{group-rank-"+group_id+"}", group_role_from_group)
+            template = template.replace("{group-rank-" + group_id + "}", group_role_from_group)
 
         if "smart-name" in template:
             if roblox_user.display_name != roblox_user.username:
@@ -80,45 +92,44 @@ async def parse_nickname(member: hikari.Member, guild: hikari.Guild, template: s
         else:
             smart_name = ""
 
-        template = template.replace(
-            "roblox-name", roblox_user.username
-        ).replace(
-            "display-name", roblox_user.display_name,
-        ).replace(
-            "smart-name", smart_name,
-        ).replace(
-            "roblox-id", str(roblox_user.id)
-        ).replace(
-            "roblox-age", str(roblox_user.age)
-        # ).replace(
-        #     "roblox-join-date", roblox_user.join_date
-        ).replace(
-            "group-rank", group_role
+        template = (
+            template.replace("roblox-name", roblox_user.username)
+            .replace(
+                "display-name",
+                roblox_user.display_name,
+            )
+            .replace(
+                "smart-name",
+                smart_name,
+            )
+            .replace("roblox-id", str(roblox_user.id))
+            .replace(
+                "roblox-age",
+                str(roblox_user.age)
+                # ).replace(
+                #     "roblox-join-date", roblox_user.join_date
+            )
+            .replace("group-rank", group_role)
         )
 
     else:
         if not template:
-            template: str | None = (await bloxlink.fetch_guild_data(guild, "unverifiedNickname")).unverifiedNickname
+            template: str | None = (
+                await bloxlink.fetch_guild_data(guild, "unverifiedNickname")
+            ).unverifiedNickname
 
             if template == "{disable-nicknaming}":
                 return
 
-    template = template.replace(
-        "discord-name", member.user.username
-    ).replace(
-        "discord-nick", member.display_name
-    ).replace(
-        "discord-mention", member.user.mention
-    ).replace(
-        "discord-id", str(member.id)
-    ).replace(
-        "server-name", guild.name
-    ).replace(
-        "prefix", "/"
-    ).replace(
-        "group-url", group.url if group else ""
-    ).replace(
-        "group-name", group.name if group else ""
+    template = (
+        template.replace("discord-name", member.user.username)
+        .replace("discord-nick", member.display_name)
+        .replace("discord-mention", member.user.mention)
+        .replace("discord-id", str(member.id))
+        .replace("server-name", guild.name)
+        .replace("prefix", "/")
+        .replace("group-url", group.url if group else "")
+        .replace("group-name", group.name if group else "")
     )
 
     for outer_nick in nickname_template_regex.findall(template):
@@ -144,15 +155,20 @@ async def parse_nickname(member: hikari.Member, guild: hikari.Guild, template: s
 
                 template = template.replace("{{{0}}}".format(outer_nick), nick_value)
             else:
-                template = template.replace("{{{0}}}".format(outer_nick), outer_nick) # remove {} only
+                template = template.replace("{{{0}}}".format(outer_nick), outer_nick)  # remove {} only
         else:
             template = template.replace("{{{0}}}".format(outer_nick), nick_value)
-
 
     return template[:32] if is_nickname else template
 
 
-async def apply_binds(member: hikari.Member, guild_id: hikari.Snowflake, roblox_account: users.RobloxAccount=None, *, moderate_user=False) -> hikari.Embed:
+async def apply_binds(
+    member: hikari.Member,
+    guild_id: hikari.Snowflake,
+    roblox_account: users.RobloxAccount = None,
+    *,
+    moderate_user=False,
+) -> hikari.Embed:
     if roblox_account and roblox_account.groups is None:
         await roblox_account.sync(["groups"])
 
@@ -164,24 +180,25 @@ async def apply_binds(member: hikari.Member, guild_id: hikari.Snowflake, roblox_
             member_roles[role.id] = {
                 "id": role.id,
                 "name": role.name,
-                "managed": bool(role.bot_id) and role.name != "@everyone"
+                "managed": bool(role.bot_id) and role.name != "@everyone",
             }
 
-    user_binds, user_binds_response = await fetch("POST", f"{BOT_API}/binds/{member.id}", headers={"Authorization":BOT_API_AUTH}, body={
-        "guild": {
-            "id": guild.id,
-            "roles": [{
-                "id": r.id,
-                "name": r.name,
-                "managed": bool(r.bot_id) and role.name != "@everyone"
-            } for r in guild.roles.values()]
+    user_binds, user_binds_response = await fetch(
+        "POST",
+        f"{BOT_API}/binds/{member.id}",
+        headers={"Authorization": BOT_API_AUTH},
+        body={
+            "guild": {
+                "id": guild.id,
+                "roles": [
+                    {"id": r.id, "name": r.name, "managed": bool(r.bot_id) and role.name != "@everyone"}
+                    for r in guild.roles.values()
+                ],
+            },
+            "member": {"id": member.id, "roles": member_roles},
+            "roblox_account": roblox_account.to_dict() if roblox_account else None,
         },
-        "member": {
-            "id": member.id,
-            "roles": member_roles
-        },
-        "roblox_account": roblox_account.to_dict() if roblox_account else None
-    })
+    )
 
     if user_binds_response.status == 200:
         user_binds = user_binds["binds"]
@@ -190,14 +207,13 @@ async def apply_binds(member: hikari.Member, guild_id: hikari.Snowflake, roblox_
 
     # first apply the required binds, then ask the user if they want to apply the optional binds
 
-    #add_roles:    set = set() # used exclusively for display purposes
-    add_roles:    list[hikari.Role] = []
+    # add_roles:    set = set() # used exclusively for display purposes
+    add_roles: list[hikari.Role] = []
     remove_roles: list[hikari.Role] = []
     possible_nicknames: list[list[hikari.Role | str]] = []
     warnings: list[str] = []
     chosen_nickname = None
     applied_nickname = None
-
 
     for required_bind in user_binds["required"]:
         # find valid roles from the server
@@ -213,16 +229,13 @@ async def apply_binds(member: hikari.Member, guild_id: hikari.Snowflake, roblox_
             if role := guild.roles.get(bind_remove_id):
                 remove_roles.append(role)
 
-
     # real_add_roles = add_roles
-
 
     # remove_roles   = remove_roles.difference(add_roles) # added roles get priority
     # real_add_roles = add_roles.difference(set(member.roles)) # remove roles that are already on the user, also new variable so we can achieve idempotence
 
     # if real_add_roles or remove_roles:
     #     await bloxlink.edit_user_roles(member, guild_id, add_roles=real_add_roles, remove_roles=remove_roles)
-
 
     if possible_nicknames:
         if len(possible_nicknames) == 1:
@@ -248,7 +261,9 @@ async def apply_binds(member: hikari.Member, guild_id: hikari.Snowflake, roblox_
             #     raise RuntimeError(f"Nickname API returned an error: {chosen_nickname_http}")
 
             if guild.owner_id == member.id:
-                warnings.append(f"Since you're the Server Owner, I cannot modify your nickname.\nNickname: {chosen_nickname}")
+                warnings.append(
+                    f"Since you're the Server Owner, I cannot modify your nickname.\nNickname: {chosen_nickname}"
+                )
             else:
                 try:
                     await member.edit(nickname=chosen_nickname)
@@ -261,9 +276,11 @@ async def apply_binds(member: hikari.Member, guild_id: hikari.Snowflake, roblox_
         if add_roles or remove_roles:
             # since this overwrites their roles, we need to add in their current roles
             # then, we remove the remove_roles from the set
-            await member.edit(roles=set(getattr(r, "id", r)
-                                for r in add_roles + member.role_ids) \
-                                    .difference([r.id for r in remove_roles]))
+            await member.edit(
+                roles=set(getattr(r, "id", r) for r in add_roles + member.role_ids).difference(
+                    [r.id for r in remove_roles]
+                )
+            )
 
     except hikari.errors.ForbiddenError:
         raise BloxlinkForbidden("I don't have permission to add roles to this user.")
@@ -272,38 +289,25 @@ async def apply_binds(member: hikari.Member, guild_id: hikari.Snowflake, roblox_
         embed = hikari.Embed(
             title="Member Updated",
         )
-        embed.set_author(name=str(member),
-                        icon=member.display_avatar_url.url,
-                        url=roblox_account.profile_link if roblox_account else None)
-
+        embed.set_author(
+            name=str(member),
+            icon=member.display_avatar_url.url,
+            url=roblox_account.profile_link if roblox_account else None,
+        )
 
         if add_roles:
-            embed.add_field(
-                name="Added Roles",
-                value=",".join([r.mention for r in add_roles])
-            )
+            embed.add_field(name="Added Roles", value=",".join([r.mention for r in add_roles]))
 
         if remove_roles:
-            embed.add_field(
-                name="Removed Roles",
-                value=",".join([r.mention for r in remove_roles])
-            )
+            embed.add_field(name="Removed Roles", value=",".join([r.mention for r in remove_roles]))
 
         if applied_nickname:
-            embed.add_field(
-                name="Nickname Changed",
-                value=applied_nickname
-            )
+            embed.add_field(name="Nickname Changed", value=applied_nickname)
 
         if warnings:
-            embed.add_field(
-                name=f"Warning{'s' if len(warnings) >= 2 else ''}",
-                value="\n".join(warnings)
-            )
+            embed.add_field(name=f"Warning{'s' if len(warnings) >= 2 else ''}", value="\n".join(warnings))
 
     else:
-        embed = hikari.Embed(
-            description="No binds apply to you!"
-        )
+        embed = hikari.Embed(description="No binds apply to you!")
 
     return embed
