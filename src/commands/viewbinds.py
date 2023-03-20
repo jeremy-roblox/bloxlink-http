514a82d31df8f63fc510ac495be37b26a3478499
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-20 04:45:26 +0000

    Structure response handling methods & layouts

--- src/commands/viewbinds.py
+++ src/commands/viewbinds.py
@@ -33,8 +33,52 @@ async def id_autocomplete_handler(interaction: hikari.AutocompleteInteraction):
     ],
 )
 class ViewBindsCommand:
-    """View the binds setup in your server."""
+    """View your binds for your server."""
 
     async def __main__(self, ctx: CommandContext):
-        await ctx.response.send("WIP.")
+        category = ctx.options["category"]
+        id_option = ctx.options["id"]
+
+        embed = hikari.Embed()
+        components = None
+
+        # Valid categories:
+        #   - Group
+        #   - Asset
+        #   - Badge
+        #   - Gamepass
+        match category.lower():
+            case "group":
+                embed, components = self.group_response(ctx, id_option)
+
+            case "asset":
+                embed, components = self.asset_response(ctx, id_option)
+
+            case "badge":
+                embed, components = self.badge_response(ctx, id_option)
+
+            case "gamepass":
+                embed, components = self.gamepass_response(ctx, id_option)
+
+            case _:
+                embed.description = (
+                    "Your given category option was invalid. "
+                    "Only `Group`, `Asset`, `Badge`, and `Gamepass` are allowed options."
+                )
+
+        await ctx.response.send(embed=embed)
+
+    # This method isn't very "dry", since each method effectively makes the embed and
+    # sets up any components as necessary. Might be worth reconsidering how to handle this better.
+
+    async def group_response(self, ctx: CommandContext, id_filter: str):
+        return
+
+    async def asset_response(self, ctx: CommandContext, id_filter: str):
+        return
+
+    async def badge_response(self, ctx: CommandContext, id_filter: str):
+        return
+
+    async def gamepass_response(self, ctx: CommandContext, id_filter: str):
         return
