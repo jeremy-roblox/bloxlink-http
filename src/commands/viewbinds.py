052a0e7656b925ad7c877836623a8b99191fae7c
Nub <38899321+One-Nub@users.noreply.github.com>
2023-03-24 04:46:00 +0000

    Slim down viewbinds.py, change arg names for clarity

--- src/commands/viewbinds.py
+++ src/commands/viewbinds.py
@@ -50,58 +50,35 @@ class ViewBindsCommand:
         #   - Badge
         #   - Gamepass
 
-        # match category.lower():
-        #     case "group":
-        #         embed, components = self.group_response(ctx, id_option)
-
-        #     case "asset":
-        #         embed, components = self.asset_response(ctx, id_option)
-
-        #     case "badge":
-        #         embed, components = self.badge_response(ctx, id_option)
-
-        #     case "gamepass":
-        #         embed, components = self.gamepass_response(ctx, id_option)
-
-        #     case _:
-        #         embed.description = (
-        #             "Your given category option was invalid. "
-        #             "Only `Group`, `Asset`, `Badge`, and `Gamepass` are allowed options."
-        #         )
-        page = await self.build_page(ctx, category.lower(), 0)
+        page = None
+        if id_option == "View binds":
+            page = await self.build_page(ctx, category.lower(), page_number=0)
+        else:
+            page = await self.build_page(ctx, category.lower(), page_number=0, id_filter=id_option)
         if not page:
-            page = "No binds found"
+            page = "You have no binds that match the options you passed. "
+            "Please use `/bind` to make a new role bind, or try again with different options."
 
         embed.description = page
         await ctx.response.send(embed=embed)
 
-    # This method isn't very "dry", since each method effectively makes the embed and
-    # sets up any components as necessary. Might be worth reconsidering how to handle this better.
-
-    async def group_response(self, ctx: CommandContext, id_filter: str):
-        embed = hikari.Embed()
-        embed.title = "Bloxlink Group Bindings"
-        return
-
-    async def asset_response(self, ctx: CommandContext, id_filter: str):
-        return
-
-    async def badge_response(self, ctx: CommandContext, id_filter: str):
-        return
-
-    async def gamepass_response(self, ctx: CommandContext, id_filter: str):
-        return
-
     # Arbitrarily choosing that 10 binds per page should be good.
     async def build_page(
-        self, ctx: CommandContext, category: str, new_page_index: int, id_filter: str = None
+        self, ctx: CommandContext, category: str, page_number: int, id_filter: str = None
     ) -> str:
         guild_data = await bloxlink.fetch_guild_data(ctx.guild_id, "binds")
 
         print(guild_data.binds)
         print(guild_data.binds[0]["bind"])
 
-        # Fiter for the category.
+        # Filter for the category.
+        categories = ("group", "asset", "badge", "gamepass")
+        if category not in categories:
+            return (
+                "Your given category option was invalid. "
+                "Only `Group`, `Asset`, `Badge`, and `Gamepass` are allowed options."
+            )
+
         binds = filter(lambda b: b["bind"]["type"] == category, guild_data.binds)
         if id_filter:
             binds = filter(lambda b: b["bind"]["id"] == id_filter, binds)
@@ -115,16 +92,19 @@ class ViewBindsCommand:
 
         output = []
 
-        offset = new_page_index * MAX_BINDS_PER_PAGE
+        offset = page_number * MAX_BINDS_PER_PAGE
         max_count = (
             bind_length if (offset + MAX_BINDS_PER_PAGE >= bind_length) else offset + MAX_BINDS_PER_PAGE
         )
         sliced_binds = binds[offset:max_count]
 
         for element in sliced_binds:
-            bindID = element["bind"]["id"]
-            nickname = element["nickname"]
-            roles = element["roles"]
+            bind = element["bind"]
+            bindID = bind["id"]
+
+            nickname = element.get("nickname")
+            roles = element.get("roles", [])
+            remove_roles = element.get("removeRoles", [])
 
             # TODO: Helpers for logic such as showing names of id items + role names
             if category == "group":
