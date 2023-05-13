332fdc0e452116a2a127645f848c65403af0d6c2
Julien Kmec <me@julien.dev>
2023-05-13 15:32:57 -0400

    Revert "auto-reloading"
    This reverts commit 4188d13bdbf3bd293cce5db13718fc2faa7d6d38.

--- src/bot.py
+++ src/bot.py
@@ -14,29 +14,28 @@ logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
 
-bot = Bloxlink(
-    public_key=DISCORD_PUBLIC_KEY,
-    token=DISCORD_TOKEN,
-    token_type=hikari.TokenType.BOT,
-    asgi_managed=False
-)
-
-app = fastapi.FastAPI(on_startup=[bot.start, sync_commands(bot)], on_shutdown=[bot.close])
-
-bot.interaction_server.set_listener(hikari.CommandInteraction, handle_command)
-bot.interaction_server.set_listener(hikari.ComponentInteraction, handle_component)
-bot.interaction_server.set_listener(hikari.AutocompleteInteraction, handle_autocomplete)
-
-for directory in MODULES:
-    files = [name for name in listdir('src/'+directory.replace('.', '/')) if name[:1] != "." and name[:2] != "__" and name != "_DS_Store"]
-
-    for filename in [f.replace(".py", "") for f in files]:
-        if filename in ('bot', '__init__'):
-            continue
-
-        bot.load_module(f"{directory.replace('/','.')}.{filename}")
-
-app.mount("/", bot) # blocks, no other sub-apps can be registered after
-
 if __name__ == "__main__":
-    uvicorn.run("bot:app", host=env.get("HOST", SERVER_HOST), port=env.get("PORT", SERVER_PORT), reload=True)
+    bot = Bloxlink(
+        public_key=DISCORD_PUBLIC_KEY,
+        token=DISCORD_TOKEN,
+        token_type=hikari.TokenType.BOT,
+        asgi_managed=False
+    )
+
+    for directory in MODULES:
+        files = [name for name in listdir('src/'+directory.replace('.', '/')) if name[:1] != "." and name[:2] != "__" and name != "_DS_Store"]
+
+        for filename in [f.replace(".py", "") for f in files]:
+            if filename in ('bot', '__init__'):
+                continue
+
+            bot.load_module(f"{directory.replace('/','.')}.{filename}")
+
+    bot.interaction_server.set_listener(hikari.CommandInteraction, handle_command)
+    bot.interaction_server.set_listener(hikari.ComponentInteraction, handle_component)
+    bot.interaction_server.set_listener(hikari.AutocompleteInteraction, handle_autocomplete)
+
+    app = fastapi.FastAPI(on_startup=[bot.start, sync_commands(bot)], on_shutdown=[bot.close])
+    app.mount("/", bot)
+
+    uvicorn.run(app, host=env.get("HOST", SERVER_HOST), port=env.get("PORT", SERVER_PORT))
