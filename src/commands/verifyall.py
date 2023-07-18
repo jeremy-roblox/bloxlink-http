69368deaececc34087e18891b2e7ba784b144f71
Nub <38899321+One-Nub@users.noreply.github.com>
2023-07-18 21:12:59 +0000

    Handle responses from the gateway when starting the scan.

--- src/commands/verifyall.py
+++ src/commands/verifyall.py
@@ -2,6 +2,7 @@ from resources.bloxlink import instance as bloxlink
 from resources.models import CommandContext
 import hikari
 import logging
+import json
 
 logger = logging.getLogger("verify_all")
 
@@ -31,16 +32,29 @@ class VerifyallCommand:
         """
 
         try:
-            await bloxlink.relay(
-                "VERIFYALL", payload={"guild_id": ctx.guild_id, "channel_id": ctx.interaction.channel_id}
+            req = await bloxlink.relay(
+                "VERIFYALL",
+                payload={"guild_id": ctx.interaction.guild_id, "channel_id": ctx.interaction.channel_id},
             )
 
-            await ctx.response.send(content="Server will start being scanned soon:tm:.")
+            # ngl this is disgusting to do, but is required based on how .relay works.
+            data = req.get("data")
+            data = data.decode("utf-8")
+            data = json.loads(data)
+            data = data.get("data")
+
+            status = data.get("status")
+            if "error" in status:
+                errors = data.get("errors")
+                logger.error(f"Gateway response error to /verifyall: {errors}")
+                await ctx.response.send(
+                    content="There was an issue when trying to update all your server members. Try again later."
+                )
+                return
+
+            await ctx.response.send(content="Your server members will be updated shortly!")
         except (RuntimeError, TimeoutError) as ex:
             await ctx.response.send(
                 content="There was an issue when starting to scan your server. Try again later."
             )
-            logger.error(f"An issue was encountered contacting the gateway - {ex}")
-
-        # Use to confirm if the server got the request or not
-        # yes: send next resposne, no: explode the world (say there was an issue, try later)
+            logger.error(f"An issue was encountered contacting the gateway - {ex};{ex.__cause__}")
