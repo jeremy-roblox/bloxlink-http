73b7de4c0c6c6a07f9a8aea5e60a65b1e89ac65f
Nub <38899321+One-Nub@users.noreply.github.com>
2023-08-11 14:00:44 +0000

    Add docstrings, move name property to work under __str__

--- src/resources/roblox/roblox_entity.py
+++ src/resources/roblox/roblox_entity.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Literal
 
 
 @dataclass(slots=True)
 class RobloxEntity(ABC):
+    """Representation of an entity on Roblox."""
+
     id: str
     name: str = None
     description: str = None
@@ -11,17 +14,26 @@ class RobloxEntity(ABC):
 
     @abstractmethod
     async def sync(self):
+        """Sync a Roblox entity with the data from Roblox."""
         return
 
-    @property
-    def logical_name(self):
-        if self.name is None:
-            return f"*(Unknown Roblox Entity)* ({self.id})"
-        else:
-            return f"**{self.name}** ({self.id})"
+    def __str__(self) -> str:
+        name = f"**{self.name}**" if self.name else "*(Unknown Roblox Entity)*"
+        return f"{name} ({self.id})"
 
 
-def create_entity(category: str, entity_id: int) -> RobloxEntity:
+def create_entity(
+    category: Literal["asset", "badge", "gamepass", "group"] | str, entity_id: int
+) -> RobloxEntity:
+    """Create a respective Roblox entity from a category and ID.
+
+    Args:
+        category (str): Type of Roblox entity to make. Subset from asset, badge, group, gamepass.
+        entity_id (int): ID of the entity on Roblox.
+
+    Returns:
+        RobloxEntity: The respective RobloxEntity implementer, unsynced.
+    """
     match category:
         case "asset":
             from resources.roblox.assets import RobloxAsset
