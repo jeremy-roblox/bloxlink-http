676dec446001c0cf2dce75e3ab74e6b008925db0
Nub <38899321+One-Nub@users.noreply.github.com>
2023-09-23 03:33:36 +0000

    Implement requested changes + update arguments

--- src/commands/reversesearch.py
+++ src/commands/reversesearch.py
@@ -1,16 +1,11 @@
 import hikari
 from hikari import Embed
 from hikari.commands import CommandOption, OptionType
-from hikari.errors import NotFoundError
 
 import resources.roblox.users as users
 from resources.bloxlink import instance as bloxlink
-from resources.exceptions import (  # pylint: disable=import-error, no-name-in-module
-    BloxlinkException,
-    RobloxNotFound,
-)
+from resources.exceptions import RobloxNotFound
 from resources.models import CommandContext
-from resources.utils import fetch
 
 
 @bloxlink.command(
@@ -19,14 +14,14 @@ from resources.utils import fetch
     options=[
         CommandOption(
             type=OptionType.STRING,
-            name="target",
-            description="Please specify either a Roblox username or ID",
+            name="user",
+            description="Please specify either a Roblox username or ID to search for.",
             is_required=True,
         ),
         CommandOption(
             type=OptionType.STRING,
-            name="search_type",
-            description="Please specify if it is a username or ID.",
+            name="type",
+            description="Are you searching for a username or a roblox ID?",
             choices=[
                 hikari.CommandChoice(name="Username", value="username"),
                 hikari.CommandChoice(name="ID", value="ID"),
@@ -36,67 +31,53 @@ from resources.utils import fetch
     ],
 )
 class ReverseSearchCommand:
-    """find Discord IDs in your server that are linked to a certain Roblox ID"""
-
-    def __init__(self):
-        self.examples = ["1", "569422833", "blox_link"]
-        self.arguments = [
-            {
-                "prompt": "Please specify either a Roblox username or ID and the type.",
-                "slash_desc": "Please specify either a Roblox username or ID and the type.",
-                "name": "target",
-            }
-        ]
-        self.category = "Administration"
-        self.aliases = ["reverse-search"]
-        self.slash_enabled = True
+    """Find Discord users in your server that are linked to a certain Roblox account."""
 
     async def __main__(self, ctx: CommandContext):
         guild = ctx.guild_id
-        target = ctx.options["target"]
-        search_type = ctx.options["search_type"]
-        response = ctx.response
+        target = ctx.options["user"]
+        search_type = ctx.options["type"]
+
+        username = None if search_type != "username" else target
+        roblox_id = None if search_type != "ID" else target
+
+        if not target.isdigit() and search_type == "ID":
+            await ctx.response.send(
+                "The input you gave is not an ID! Try again with the username option chosen instead."
+            )
+            return
 
         try:
-            if search_type == "username":
-                json_data = await fetch(
-                    "POST",
-                    "https://users.roblox.com/v1/usernames/users",
-                    body={"usernames": [target], "excludeBannedUsers": False},
-                )
+            account = await users.get_user(roblox_username=username, roblox_id=roblox_id)
+        except RobloxNotFound as exc:
+            raise RobloxNotFound(
+                "The Roblox user you were searching for does not exist! "
+                f"Please check the {search_type} and try again!"
+            ) from exc
 
-                if len(json_data[0]["data"]) == 0:
-                    raise RobloxNotFound()
+        if account.id is None or account.username is None:
+            raise RobloxNotFound("The Roblox user you were searching for does not exist.")
 
-                roblox_id = str(json_data[0]["data"][0]["id"])
-            else:
-                roblox_id = target
+        results = []
 
-            account = await users.get_user(roblox_id=True and roblox_id)
-        except RobloxNotFound:
-            raise BloxlinkException("This Roblox account doesn't exist.")
-        else:
-            num_documents = await bloxlink.mongo.bloxlink.users.count_documents(
-                {"robloxAccounts.accounts": roblox_id}
-            )
+        id_list = await bloxlink.reverse_lookup(account.id)
+        for user_id in id_list:
+            user = await bloxlink.fetch_discord_member(guild, user_id, "id")
 
-            results = []
+            if user is None:
+                continue
 
-            if num_documents > 0:
-                documents = bloxlink.mongo.bloxlink.users.find({"robloxAccounts.accounts": roblox_id})
+            if isinstance(user, hikari.Member):
+                results.append(f"{user.mention} ({user.id})")
 
-                for document in await documents.to_list(length=num_documents):
-                    try:
-                        user = await bloxlink.rest.fetch_member(guild, document["_id"])
-                    except NotFoundError:
-                        pass
-                    else:
-                        results.append(f"{user.mention} ({user.id})")
+            elif isinstance(user, dict):
+                user_id = user["data"]["id"]
+                results.append(f"<@{user_id}> ({user_id})")
 
-            embed = Embed(
-                title=f"Reverse Search for {account.username}",
-                description="\n".join(results) if results else "No results found.",
-            )
-            # embed.set_thumbnail(account.avatar)
+        embed = Embed(
+            title=f"Reverse Search for {account.username}",
+            description="\n".join(results) if results else "No results found.",
+        )
+        embed.set_thumbnail(account.avatar)
 
-            await ctx.response.send(embed=embed)
+        await ctx.response.send(embed=embed)
