3605d70bfda3cc3a923ca9d1d8c912746da14ad6
Nub <38899321+One-Nub@users.noreply.github.com>
2023-12-28 21:50:09 +0000

    Pass along all args & kwargs for authenticate() wrapper

--- src/web/decorators.py
+++ src/web/decorators.py
@@ -1,8 +1,10 @@
-from functools import wraps
-from blacksheep import Request, unauthorized
-from resources.secrets import SERVER_AUTH  # pylint: disable=no-name-in-module
-from blacksheep.server.normalization import ensure_response
 import logging
+from functools import wraps
+
+from blacksheep import Request, unauthorized
+from blacksheep.server.normalization import ensure_response
+
+from resources.secrets import SERVER_AUTH  # pylint: disable=no-name-in-module
 
 logger = logging.getLogger()
 
@@ -12,20 +14,26 @@ UNAUTHORIZED_RESPONSE = unauthorized("You are not authorized to use this endpoin
 def authenticate():
     def decorator(handler):
         @wraps(handler)
-        async def wrapped(endpoint, request: Request):
-            auth_header = request.get_first_header(b"Authorization").decode() if request.has_header(b"Authorization") else None
-
+        async def wrapped(*args, **kwargs):
             # In case we ever omit the server auth config, we forbid all requests.
             if not SERVER_AUTH:
                 logger.error("No SERVER_AUTH was set! Blocking all requests.")
                 return UNAUTHORIZED_RESPONSE
 
+            # Find the Request typed argument. Will not work if the handler does not want the Request obj.
+            request = next((arg for arg in args if isinstance(arg, Request)), None)
+            if request is None:
+                return UNAUTHORIZED_RESPONSE
+
+            auth_header = (
+                request.get_first_header(b"Authorization").decode()
+                if request.has_header(b"Authorization")
+                else None
+            )
             if auth_header != SERVER_AUTH:
                 return UNAUTHORIZED_RESPONSE
 
-            response = ensure_response(await handler(endpoint, request))
-
-            return response
+            return ensure_response(await handler(*args, **kwargs))
 
         return wrapped
 
