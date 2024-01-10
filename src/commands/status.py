5c8804f17880401fac97af30a5c7ff8aa0a0b89b
Julien Kmec <me@julien.dev>
2024-01-10 00:39:27 -0500

    show upsell button in /status

--- src/commands/status.py
+++ src/commands/status.py
@@ -3,6 +3,7 @@ from hikari import Embed
 from resources.bloxlink import instance as bloxlink
 from resources.commands import CommandContext
 from resources.premium import get_premium_status
+from resources.exceptions import PremiumRequired
 
 
 @bloxlink.command(
@@ -16,16 +17,14 @@ class StatusCommand:
         premium_status = await get_premium_status(guild_id=ctx.guild_id, interaction=ctx.interaction)
         embed = Embed()
 
-        if premium_status.active:
-            embed.description = ":heart: Thanks for being a **premium subscriber!**"
-            embed.color = 0xFDC333
-            embed.add_field(name="Premium Status", value="Active", inline=True)
-            embed.add_field(name="Tier", value=premium_status.tier, inline=True)
-            embed.add_field(name="Payment Source", value=premium_status.payment_name_url, inline=True)
-            embed.add_field(name="Unlocked Features", value=str(premium_status), inline=True)
-        else:
-            embed.description = (
-                "This server does not have premium. A server admin may purchase it [here](https://blox.link)."
-            )
+        if not premium_status.active:
+            raise PremiumRequired()
+
+        embed.description = ":heart: Thanks for being a **premium subscriber!**"
+        embed.color = 0xFDC333
+        embed.add_field(name="Premium Status", value="Active", inline=True)
+        embed.add_field(name="Tier", value=premium_status.tier, inline=True)
+        embed.add_field(name="Payment Source", value=premium_status.payment_name_url, inline=True)
+        embed.add_field(name="Unlocked Features", value=str(premium_status), inline=True)
 
         return await ctx.response.send_first(embed=embed)
